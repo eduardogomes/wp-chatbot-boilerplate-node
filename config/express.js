98b48726527c71f3eca5e0a398469c7709cbe2ee
Eduardo Gomes <edugomes@fb.com>
2017-10-24 19:29:56 -0200

    Including x-hub-verification validation

--- config/express.js
+++ config/express.js
@@ -47,13 +47,4 @@ module.exports = function(app, config) {
     });
   }
 
-  app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-      res.render('error', {
-        message: err.message,
-        error: {},
-        title: 'error'
-      });
-  });
-
 };
